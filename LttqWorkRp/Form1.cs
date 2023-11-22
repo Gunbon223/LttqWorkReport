ceb05aeeb8990d0d11d92b2e7ff9c8b6a08a01ea
Gunbon223 <88621181+Gunbon223@users.noreply.github.com>
2023-11-22 15:02:47 +0700

    Add project files.

---
+++ LttqWorkRp/Form1.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Reporting.WinForms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LttqWorkRp
+{
+	public partial class Form1 : Form
+	{
+		ProcessDatabase pd = new ProcessDatabase();
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
+            this.reportViewer1.RefreshReport();
+			reportViewer1.LocalReport.ReportEmbeddedResource =
+			"LttqWorkRp.Report1.rdlc";
+			ReportDataSource rpd = new ReportDataSource();
+			rpd.Name = "DataSet1";
+			rpd.Value = pd.DocBang("select * from MonAn");
+			reportViewer1.LocalReport.DataSources.Add(rpd);
+			this.reportViewer1.RefreshReport();
+		}
+
+	
+
+    }
+}
